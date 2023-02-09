1938c4e4284ffed2896c17a3eb253fb90e7e86c7
DON <don@DONs-Laptop-9.local>
2023-02-09 16:17:14 -0500

    init w sam

---
+++ infrastructure/src/lambda/pre-signed/app.ts
@@ -0,0 +1,32 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from 'aws-lambda';
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+export const lambdaHandler: Handler = async (event : APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const client = new S3Client({})
+    console.log(event)
+    const params = { Bucket: "indexed-submission-bucket", Key: "test.txt" }
+    const command = new PutObjectCommand(params)
+    const res: string = await getSignedUrl(client, command, {
+        expiresIn: 60 * 60,
+    })
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: res,
+        }),
+    };
+
+   return response
+}
