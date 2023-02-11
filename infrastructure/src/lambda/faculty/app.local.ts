b005fe6e4180e80d8eedd3b48c268bb9c4fbdea2
DON <don@DONs-Laptop-9.local>
2023-02-11 13:33:23 -0500

    fixing namespace issue

--- infrastructure/src/lambda/faculty/app.local.ts
+++ infrastructure/src/lambda/faculty/app.local.ts
@@ -1,8 +1,8 @@
-namespace FacultyLocalApp {
+
     const facultyApp = require("./app")
     const SEVER_PORT = 8080
 
     facultyApp.listen(SEVER_PORT, () => {
         console.log(`Server is running on port ${SEVER_PORT}`)
     })
-}
+
