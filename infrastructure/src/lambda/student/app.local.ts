e114000b2ec7e07d88e2e834542703a8cbfbc08c
DON <don@DONs-Laptop-9.local>
2023-02-11 20:25:20 -0500

    minor fixes

--- infrastructure/src/lambda/student/app.local.ts
+++ infrastructure/src/lambda/student/app.local.ts
@@ -1,8 +1,10 @@
-const { devConnect } = require('./app')
+const { devConnect } = require("./app")
 const studentApp = require("./app")
 const PORT = 8080
 studentApp.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`)
 })
 
-devConnect("dev").then((e: string) => { console.log(e) })
+devConnect("dev").then((e: string) => {
+    console.log(e)
+})
--- infrastructure/src/lambda/student/app.ts
+++ infrastructure/src/lambda/student/app.ts
@@ -3,25 +3,25 @@ const studentExpressApp = x()
 const parse = require("body-parser")
 const dynamoose = require("dynamoose")
 studentExpressApp.use(parse.json())
-const catModel = require('./model/student.schema')
+const catModel = require("./model/student.schema")
 
 studentExpressApp.get("/student/hello", async function (req: any, res: any) {
     return res.send("Hello Student")
 })
 studentExpressApp.get("/student/deploy", async function (req: any, res: any) {
     return res.json({
-                hash: await new catModel({
-                    userId: "1",
-                    age: 13,
-                    email : "tochey@outlook.com"
-                }).save()
-            })
+        hash: await new catModel({
+            userId: "1",
+            age: 13,
+            email: "tochey@outlook.com",
+        }).save(),
+    })
 })
 
 module.exports = studentExpressApp
 module.exports.devConnect = async (env: string): Promise<string> => {
     if (env === "dev") {
-        await dynamoose.aws.ddb.local();
+        await dynamoose.aws.ddb.local()
     }
 
     return "Connected to dev dynamo"
